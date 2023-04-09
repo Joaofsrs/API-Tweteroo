dd98bfc9eefc06cfa4e91c41f499d74e043dc614
Joaofsrs <rsjoao12@gmail.com>
2023-04-09 11:33:00 -0300

    paginação Get usando query

--- src/app.js
+++ src/app.js
@@ -38,24 +38,54 @@ app.post("/tweets",(req, res) => {
 });
 
 app.get("/tweets",(req, res) => {
+    const page = Number(req.query.page);
     const lastTweets = [];
     const tweetsSize = tweets.length;
 
-    for(let i = 1; i <= 10; i++){
-        if(tweetsSize - i >= 0){
-            const user = users.find((user) => user.username === tweets[tweetsSize - i].username);
-            const tweet = { 
-                username: tweets[tweetsSize - i].username,
-                avatar: user.avatar,
-                tweet: tweets[tweetsSize - i].tweet
-            };
-            lastTweets.push(tweet);
-        }else{
-            break
-        }
-    }
 
-    res.status(201).send(lastTweets);
+    if(page){
+        let start = 0;
+        let end  = 10;
+        if(page < 1){
+            res.status(400).send("Informe uma página válida!");
+            return;
+        }
+        if(page > 1){
+            start = 10*(page-1)
+            end = 10*page;
+        }
+        for(let i = start; i < end; i++){
+            if(i < tweetsSize){
+                const user = users.find((user) => user.username === tweets[i].username);
+                const tweet = { 
+                    username: tweets[i].username,
+                    avatar: user.avatar,
+                    tweet: tweets[i].tweet
+                };
+                lastTweets.push(tweet);
+            }else{
+                break
+            }
+        }
+    
+        res.status(201).send(lastTweets);
+    }else{
+        for(let i = 1; i <= 10; i++){
+            if(tweetsSize - i >= 0){
+                const user = users.find((user) => user.username === tweets[tweetsSize - i].username);
+                const tweet = { 
+                    username: tweets[tweetsSize - i].username,
+                    avatar: user.avatar,
+                    tweet: tweets[tweetsSize - i].tweet
+                };
+                lastTweets.push(tweet);
+            }else{
+                break
+            }
+        }
+    
+        res.status(201).send(lastTweets);
+    }
 });
 
 app.get("/tweets/:username", (req, res) => {
